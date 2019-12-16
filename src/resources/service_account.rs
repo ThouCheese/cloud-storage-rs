32b290cd0dc3a5ca65e94b4534440a43168b612a
ThouCheese <luuk.wester@gmail.com>
2019-12-16 13:48:20 +0100

    added docs to service account

--- src/resources/service_account.rs
+++ src/resources/service_account.rs
@@ -1,3 +1,5 @@
+/// The struct is the parsed service account json file. It is publicly exported to enable easier
+/// debugging of which service account is currently used.
 #[derive(serde::Deserialize, Debug)]
 pub struct ServiceAccount {
     #[serde(rename = "type")]
