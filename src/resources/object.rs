7ed27e079de632e3fb1393eda5b8b63a492b29db
cboudereau <cboudereau@gmail.com>
2021-09-29 11:40:50 +0200

    clean

--- src/resources/object.rs
+++ src/resources/object.rs
@@ -4,7 +4,7 @@ use futures::Stream;
 #[cfg(feature = "global-client")]
 use futures::TryStream;
 use percent_encoding::{utf8_percent_encode, AsciiSet, NON_ALPHANUMERIC};
-use std::collections::HashMap;
+use std::{collections::HashMap, convert::TryInto};
 
 /// A resource representing a file in Google Cloud Storage.
 #[derive(Debug, PartialEq, serde::Serialize, serde::Deserialize)]
@@ -239,16 +239,43 @@ pub struct ObjectList {
 /// Partial Response from `Object::list`.
 #[derive(Debug, serde::Deserialize, Default)]
 #[serde(rename_all = "camelCase")]
-pub struct PartialObjectList {
+pub struct PartialObjectList<T> {
+    /// The kind of item this is. For lists of objects, this is always `storage#objects`.
+    pub kind: Option<String>,
     /// The list of objects, ordered lexicographically by name.
     #[serde(default = "Vec::new")]
-    pub items: Vec<PartialObject>,
+    pub items: Vec<T>,
+
+    /// Object name prefixes for objects that matched the listing request but were excluded
+    /// from `items` because of a delimiter. Values in this list are object names up to and
+    /// including the requested delimiter. Duplicate entries are omitted from this list.
+    #[serde(default = "Vec::new")]
+    pub prefixes: Vec<String>,
 
     /// The continuation token, included only if there are more items to return. Provide
     /// this value as the `page_token` of a subsequent request in order to return the next
     /// page of results.
     pub next_page_token: Option<String>,
 }
+
+impl TryInto<ObjectList> for PartialObjectList<Object> {
+    type Error = String;
+
+    fn try_into(self) -> Result<ObjectList, Self::Error> {
+        match self.kind {
+            None => {
+                Err("can't convert PartialObjectList to ObjectList: missing kind field".to_owned())
+            }
+            Some(k) => Ok(ObjectList {
+                kind: k.to_owned(),
+                items: self.items,
+                prefixes: self.prefixes,
+                next_page_token: self.next_page_token,
+            }),
+        }
+    }
+}
+
 #[derive(Debug, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub(crate) struct RewriteResponse {
